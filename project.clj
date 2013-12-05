80ec8db698f5afeaf13d4a1afdd2610572b16fc7
molst <molst@yahoo.com>
2013-12-05 21:30:37 +0100

    added scm info

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]}}
+  :scm {:name "git" :url "https://github.com/molst/torpo"})
