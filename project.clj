a62cd737504919b2a8d640e9ff5d569dbd2dc7d7
molst <molst@yahoo.com>
2013-12-05 21:53:08 +0100

    project file updates

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject torpo "0.1-SNAPSHOT"
+(defproject torpo "0.2-SNAPSHOT"
   :description "A minimalistic helper library."
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
+  :url "https://github.com/molst/torpo"
+  :scm {:name "git" :url "https://github.com/molst/torpo"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[midje "1.5.0"]]}}
-  :scm {:name "git" :url "https://github.com/molst/torpo"})
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
