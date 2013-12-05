c1a8cf11693b0c066254da44c0be3a9a615d9a94
molst <molst@yahoo.com>
2013-12-05 20:49:18 +0100

    no snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject torpo "0.1-SNAPSHOT"
+(defproject torpo "0.1"
   :description "A minimalistic helper library."
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
